0f0e2c072b9fd82a1d72f3f6990d1e1ed9279d5f
Peter <peter_eid@live.com>
2017-07-21 17:37:21 +0300

    .skdfm.ds;fljf.kdfjglefkjg

--- soajs.cd.js
+++ soajs.cd.js
@@ -1,122 +1,135 @@
 "use strict";
 
 var gitRepo = null;
+var gitOwner = null;
 var gitBranch = null;
+var ciProvider = null;
+var gitCommit = null;
 
 var authKey = process.env.SOAJS_CD_AUTH_KEY;
 var deployToken = process.env.SOAJS_CD_DEPLOY_TOKEN;
 var dashboardDomain = process.env.SOAJS_CD_DASHBOARD_DOMAIN;
 var dashboardProtocol = process.env.SOAJS_CD_DASHBOARD_PROTOCOL || 'https';
 var dashboardPort = process.env.SOAJS_CD_DASHBOARD_PORT || '443';
-var dashboardAPIRoute = process.env.SOAJS_CD_API_ROUTE;
+var dashboardAPIRoute= process.env.SOAJS_CD_API_ROUTE;
 
 
 var config = null;
 var request = require("request");
 
 var utils = {
-	"init": function (cb) {
+	"init": function (cb){
 		console.log("Initializing CD script");
 		//check the build environment
-		if (process.env.TRAVIS) {
+		if(process.env.TRAVIS){
 			console.log("Travis build environment detected");
-			
-			if (!process.env.TRAVIS_REPO_SLUG || !process.env.TRAVIS_BRANCH) {
+			ciProvider = 'travis';
+
+			if(!process.env.TRAVIS_REPO_SLUG || !process.env.TRAVIS_BRANCH){
 				console.log("Could not find Travis environment variables (Repo Slug | branch). Aborting");
 				process.exit(0);
 			}
-			
-			gitRepo = process.env.TRAVIS_REPO_SLUG.split("/")[1];
-			gitBranch = process.env.TRAVIS_BRANCH;
+
+			var repoSlug = process.env.TRAVIS_REPO_SLUG.split("/");
+			gitOwner = repoSlug[0].toLowerCase();
+			gitRepo = repoSlug[1].toLowerCase();
+			gitBranch = process.env.TRAVIS_BRANCH.toLowerCase();
+			gitCommit = process.env.TRAVIS_COMMIT;
 		}
-		else if (process.env.DRONE) {
+		else if(process.env.DRONE){
 			console.log("Drone build environment detected");
-			
-			if (!process.env.DRONE_REPO_NAME || !process.env.DRONE_REPO_BRANCH) {
+			ciProvider = 'drone';
+
+			if(!process.env.DRONE_REPO_NAME || !process.env.DRONE_REPO_BRANCH){
 				console.log("Could not find Drone environment variables (Repo name | branch). Aborting");
 				process.exit(0);
 			}
-			
-			gitRepo = process.env.DRONE_REPO_NAME;
-			gitBranch = process.env.DRONE_REPO_BRANCH;
+
+			gitOwner = process.env.DRONE_REPO_OWNER.toLowerCase();
+			gitRepo = process.env.DRONE_REPO_NAME.toLowerCase();
+			gitBranch = process.env.DRONE_REPO_BRANCH.toLowerCase();
+			gitCommit = process.env.DRONE_COMMIT;
 		}
 		else {
 			console.log("Could not find any build environment. Aborting...");
 			process.exit(0);
 		}
-		
+
 		//Check if required envs are set
 		console.log("Checking if required environment variables are set")
 		//check auth env variables
-		if (!authKey || !deployToken) {
+		if(!authKey || !deployToken){
 			console.log("Error: Missing AUTH env variables. Aborting...");
 			process.exit(0);
 		}
 		//check dashboard env variables
-		if (!dashboardDomain || !dashboardAPIRoute) {
+		if(!dashboardDomain || !dashboardAPIRoute){
 			console.log("Error: Missing DASHBOARD environment variables. Aborting...");
 			process.exit(0);
 		}
-		
+
 		console.log("Launching CD call...");
-		utils.createRequest(function (params) {
+		utils.createRequest(function(params){
 			request.post(params, cb);
 		});
 	},
-	
-	"createRequest": function (cb) {
+
+	"createRequest": function(cb) {
 		var params = {};
-		
+
 		params.uri = dashboardProtocol + "://" + dashboardDomain + ":" + dashboardPort +
 			dashboardAPIRoute + "?deploy_token=" + deployToken;
-		
+
 		params.headers = {
-			"key": authKey,
+			"key" : authKey,
 			"Content-Type": "application/json"
 		};
-		
+
 		params.json = true;
-		
+
 		try {
 			config = require("./config.js");
 		}
-		catch (e) {
+		catch(e) {
 			console.log("Could not find a config.js file, searching for custom config file [soa.js] ...");
-			
+
 			try {
 				config = require('./soa.js');
 			}
-			catch (e) {
+			catch(e) {
 				console.log("Could not find a soa.js file, repo does contain a service code...");
 			}
 		}
-		
+
 		params.body = {
-			"repo": gitRepo,
-			"branch": gitBranch
+			"repo" : gitRepo,
+			"branch": gitBranch,
+			"owner": gitOwner,
+			"ciProvider": ciProvider,
+			"commit": gitCommit
 		};
 		//if not a multi repo
-		if (config && config.type && config.type !== "multi" && config.serviceName) {
+		if(config && config.type && config.type !== "multi" && config.serviceName){
 			params.body.services = [{"serviceName": config.serviceName}];
-			if (config.serviceVersion) {
+			if(config.serviceVersion) {
 				params.body.services[0].serviceVersion = config.serviceVersion;
-				
+
 			}
 		}
-		
-		else if (config && config.type === "multi") {
-			
+
+		else if(config && config.type === "multi"){
+
 			//loop over each service to add its
 			var services = [];
-			config.folders.forEach(function (service) {
+			config.folders.forEach(function(service){
 				var serviceConfigPath = "./" + service + "/config.js";
 				var serviceConfig = require(serviceConfigPath);
 				//construct each service option
 				var oneService = {
 					"serviceName": serviceConfig.serviceName
 				};
-				
-				if (serviceConfig.serviceVersion) {
+
+				if(serviceConfig.serviceVersion){
 					oneService.serviceVersion = serviceConfig.serviceVersion;
 				}
 				services.push(oneService);
@@ -128,11 +141,11 @@ var utils = {
 };
 
 
-utils.init(function (err, response, body) {
+utils.init(function(err,response,body){
 	if (err) {
-		console.log(JSON.stringify(err, null, 2));
+		console.log(JSON.stringify (err, null, 2));
 	}
 	else {
-		console.log(JSON.stringify(body, null, 2));
+		console.log(JSON.stringify (body, null, 2));
 	}
 });
